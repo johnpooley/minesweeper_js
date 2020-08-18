ba8331b4f2f54f98131cbd270abff2dcf716c973
JohnPooley <thevinylvandal@hotmail.com>
2020-08-18 17:38:05 +0100

    all checking for bombs added

--- app.js
+++ app.js
@@ -22,6 +22,11 @@ document.addEventListener('DOMContentLoaded', () =>{
       square.classList.add(shuffledArray[i])
       grid.appendChild(square)
       squares.push(square)
+
+      // normal click
+      square.addEventListener('click', function(e){
+        click(square)
+      })
     }
 
     for(let i =0; i<squares.length; i++){
@@ -32,8 +37,14 @@ document.addEventListener('DOMContentLoaded', () =>{
       if (squares[i].classList.contains('valid')){
         if (i>0 && !isLeftEdge && squares[i-1].classList.contains('bomb')) total ++
         if (i>9 && !isRightEdge && squares[i+1].classList.contains('bomb')) total ++
+        if (i>10 && squares[i - width].classList.contains('bomb')) total++
+        if (i>11 && !isLeftEdge && squares[i-1-width].classList.contains('bomb')) total++
+        if (i<98 && !isRightEdge && squares[i+1].classList.contains('bomb')) total++
+        if (i<90 && !isLeftEdge && squares[i-1 - width].classList.contains('bomb')) total++
+        if (i<88 && !isRightEdge && squares[i+1+width].classList.contains('bomb')) total++
+        if (i<89 && squares[i + width].classList.contains('bomb')) total++
         squares[i].setAttribute('data', total)
-        console.log(squares[i]);
+
       }
     }
 
