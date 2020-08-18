a1361b98337ed7327f8cb59fd8523721478ccc6f
JohnPooley <thevinylvandal@hotmail.com>
2020-08-18 17:54:19 +0100

    fix errors with bomb checking

--- app.js
+++ app.js
@@ -5,14 +5,13 @@ document.addEventListener('DOMContentLoaded', () =>{
   let squares = []
   let bombAmount = 20
 
+
   // create Board
   function createBoard(){
     // get shuffled game array with random bombs
     const bombsArray = Array(bombAmount).fill('bomb')
     const emptyArray = Array(width*width - bombAmount).fill('valid')
-
     const gameArray = emptyArray.concat(bombsArray)
-
     const shuffledArray = gameArray.sort(()=> Math.random()-0.5)
 
 
@@ -24,11 +23,13 @@ document.addEventListener('DOMContentLoaded', () =>{
       squares.push(square)
 
       // normal click
-      square.addEventListener('click', function(e){
-        click(square)
-      })
+      // square.addEventListener('click', function(e){
+      //   click(square)
+      // })
     }
 
+
+// add numbers
     for(let i =0; i<squares.length; i++){
       let total=0
       const isLeftEdge = (i % width === 0)
@@ -36,24 +37,26 @@ document.addEventListener('DOMContentLoaded', () =>{
 
       if (squares[i].classList.contains('valid')){
         if (i>0 && !isLeftEdge && squares[i-1].classList.contains('bomb')) total ++
-        if (i>9 && !isRightEdge && squares[i+1].classList.contains('bomb')) total ++
+        if (i>9 && !isRightEdge && squares[i+1 -width].classList.contains('bomb')) total ++
         if (i>10 && squares[i - width].classList.contains('bomb')) total++
         if (i>11 && !isLeftEdge && squares[i-1-width].classList.contains('bomb')) total++
         if (i<98 && !isRightEdge && squares[i+1].classList.contains('bomb')) total++
-        if (i<90 && !isLeftEdge && squares[i-1 - width].classList.contains('bomb')) total++
+        if (i<90 && !isLeftEdge && squares[i-1 + width].classList.contains('bomb')) total++
         if (i<88 && !isRightEdge && squares[i+1+width].classList.contains('bomb')) total++
         if (i<89 && squares[i + width].classList.contains('bomb')) total++
         squares[i].setAttribute('data', total)
-
+console.log(squares[i]);
       }
     }
-
-
-
-
-
-
   }
   createBoard()
+
+
+  // click on square actions
+  // funtion click(square){
+  //   if (square.classList.contains('bomb')){
+  //     alert('Game over you loser!')
+  //   }
+  // }
 
 })
